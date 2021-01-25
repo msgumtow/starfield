e484c5d85828d8e5b92fab6d766fc8c510ca0fa1
msgumtow <71787320+msgumtow@users.noreply.github.com>
2021-01-25 14:00:47 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,24 @@
+let projectile: Sprite = null
+game.onUpdate(function () {
+    if (Math.percentChance(25)) {
+        projectile = sprites.createProjectileFromSide(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . 1 . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, 0, randint(20, 30))
+        projectile.setPosition(randint(0, scene.screenWidth()), 0)
+    }
+})
